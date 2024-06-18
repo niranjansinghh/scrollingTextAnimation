b6468aafd7f922a3ac73e91e61b42ea9557dd220
niranjansinghh <niranjansingh19489@gmail.com>
2024-06-18 21:44:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function animation(){
+window.addEventListener("wheel",function(dets){
+    if(dets.deltaY > 0){
+        gsap.to(".marque",{
+            transform:"translateX(-200%)",
+            repeat:-1,
+            duration:3,
+            ease:"none"
+        })
+        gsap.to(".marque img",{
+            rotate:180,
+           
+        })
+    }else{
+        gsap.to(".marque",{
+            transform:"translateX(0%)",
+            repeat:-1,
+            duration:3,
+            ease:"none"
+        })
+        gsap.to(".marque img",{
+            rotate:0,
+           
+        })
+    }
+})
+}
+animation();
